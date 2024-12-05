5ee8f7ce0423508566365def9eaff58950e6fc8e
ZachFontenot <fontenot.zachary1@gmail.com>
2024-12-05 12:06:05 -0500

    clean up day 5

--- scala/advent/src/main/scala/com/zf/days/Five.scala
+++ scala/advent/src/main/scala/com/zf/days/Five.scala
@@ -8,8 +8,7 @@ import com.zf.ParseHelpers.intParser
 case class Update(ups: List[Int])
 case class PrintOrder(before: Int, after: Int)
 case class ManPage(
-    before: Map[Int, List[Int]],
-    after: Map[Int, List[Int]],
+    rawRules: List[PrintOrder],
     updates: List[Update]
 )
 
@@ -60,78 +59,35 @@ object Five extends DayType[ManPage, Int] {
       }
     val orders = mapEithers(split.getOrElse("|", List.empty).map(oP.parseAll))
     val updates = mapEithers(split.getOrElse(",", List.empty).map(uP.parseAll))
-    orders.foldLeft(ManPage(Map.empty, Map.empty, updates)) {
-      case (ManPage(b, a, us), PrintOrder(before, after)) =>
-        val newB = b.updatedWith(after): v =>
-          v match {
-            case Some(lst) => Some(before :: lst)
-            case None      => Some(List(before))
-          }
-        val newA = a.updatedWith(before): v =>
-          v match {
-            case Some(lst) => Some(after :: lst)
-            case None      => Some(List(after))
-          }
-        ManPage(newB, newA, us)
-    }
-
-  def validOrdering(
-      update: List[Int],
-      befores: Map[Int, List[Int]],
-      afters: Map[Int, List[Int]]
-  ): Boolean =
-    val withIdx = update.zip(Range(0, update.size))
-    withIdx.forall { case (toCheck, idx) =>
-      val a = afters.getOrElse(toCheck, List.empty)
-      val b = befores.getOrElse(toCheck, List.empty)
-
-      withIdx.take(idx).forall { case (n, _idx) =>
-        b.contains(n) || !a.contains(n)
-      } &&
-      withIdx.drop(idx + 1).forall { case (n, _idx) =>
-        a.contains(n) || !b.contains(n)
-      }
-
-    }
+    ManPage(orders, updates)
 
   def getMiddle[A](ls: List[A]): A =
     ls(ls.size / 2)
 
+  def sortByRules(rules: List[PrintOrder], a: Int, b: Int) =
+    rules.exists { case PrintOrder(before, after) =>
+      a == before &&
+      b == after
+    }
+  def validOrder(update: List[Int], f: (a: Int, b: Int) => Boolean) =
+    update.sortWith(f) == update
+
   def partA(input: ManPage): Int =
-    val ManPage(befores, afters, updates) = input
+    val ManPage(rules, updates) = input
+    val sortupdate = (a, b) => sortByRules(rules, a, b)
     updates.foldLeft(0) { case (acc, Update(update)) =>
-      if validOrdering(update, befores, afters)
+      if validOrder(update, sortupdate)
       then acc + getMiddle(update)
       else acc
     }
 
-  def sortUpdate(
-      update: List[Int],
-      befores: Map[Int, List[Int]],
-      afters: Map[Int, List[Int]]
-  ): List[Int] =
-    val withIdx = update.zip(Range(0, update.size))
-
-    withIdx
-      .sortWith { case ((a, aidx), (b, bidx)) =>
-        val afts = afters.getOrElse(a, List.empty)
-        val befs = befores.getOrElse(a, List.empty)
-        if !befs.contains(b) && !afts.contains(b) then aidx < bidx
-        else
-          val bidxs = befs.zip(Range(0, befs.size)).toMap
-          val curIdx = befs.size
-          val aftidxs =
-            afts.zip(Range(befs.size + 1, befs.size + 1 + afts.size)).toMap
-          curIdx < bidxs.getOrElse(b, aftidxs.getOrElse(b, 100))
-      }
-      .map { case (n, _) => n }
-
   def partB(input: ManPage): Int =
-    val ManPage(befores, afters, updates) = input
+    val ManPage(rules, updates) = input
+    val f = (a, b) => sortByRules(rules, a, b)
     val incorrect = updates.filter { case Update(update) =>
-      !validOrdering(update, befores, afters)
+      !validOrder(update, f)
     }
     incorrect.foldLeft(0) { case (acc, Update(update)) =>
-      acc + getMiddle(sortUpdate(update, befores, afters))
+      acc + getMiddle(update.sortWith(f))
     }
 }
